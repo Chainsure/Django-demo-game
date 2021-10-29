13495588ee97efd54d5f73566376b1f47c3e4e5c
chenshuo zhang <zhangchenshuo4@gmail.com>
2021-10-29 15:34:53 +0800

    first web page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">New world</h1>'
+    line2 = '<img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Finews.gtimg.com%2Fnewsapp_match%2F0%2F11978201640%2F0.jpg&refer=http%3A%2F%2Finews.gtimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1638082061&t=f776a856d6a7c2fe42562b01ddc89251" width=1250 height=750>'
+    return HttpResponse(line1 + line2)
+
