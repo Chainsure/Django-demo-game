bb22b1fa9ae38c968a09bbf66850bbc98e2d3370
chenshuo zhang <zhangchenshuo4@gmail.com>
2021-10-29 14:20:58 +0800

    modified allowed hosts

--- acapp/settings.py
+++ acapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2-=qb3u2qzke$px8b!ev@0r^3+ea!+2t2e&x!-s-jg91s19)#(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.224.187.91']
 
 
 # Application definition
