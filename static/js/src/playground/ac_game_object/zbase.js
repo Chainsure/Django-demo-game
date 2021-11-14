69c59353fa128419179fa8e432a1e3c80bee266e
chenshuo zhang <zhangchenshuo4@gmail.com>
2021-11-14 23:50:50 +0800

    create an acapp

---
+++ static/js/src/playground/ac_game_object/zbase.js
@@ -0,0 +1,56 @@
+let AC_GAME_OBJECTS = [];
+class GameObjects{
+    constructor(){
+        AC_GAME_OBJECTS.push(this);
+        this.has_called_start = false;
+        this.destroyed = false;
+        this.timedelta = 0;
+    }
+
+    start(){ // call start when constructed
+
+    }
+
+    update(){ // call update every frame
+        
+    }
+
+    on_destroy(){ //call before destroy
+
+    }
+    destroy() { // destroy the object
+        this.on_destroy();
+        for(let i = 0; i < AC_GAME_OBJECTS.length; ++i)
+        {
+            if(AC_GAME_OBJECTS[i] === this)
+            {
+                AC_GAME_OBJECTS[i].destroyed = true;
+                AC_GAME_OBJECTS.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+}
+
+let last_timestamp;
+let AC_GAME_ANIMATION = function(timestamp){
+    for(let i = 0; i < AC_GAME_OBJECTS.length; i++)
+    {
+        let obj = AC_GAME_OBJECTS[i];
+        if(!obj.has_called_start)
+        {
+            obj.has_called_start = true;
+            obj.start();
+        }
+        else
+        {
+            obj.timedelta = timestamp - last_timestamp;
+            obj.update();
+        }
+    }
+    last_timestamp = timestamp;
+    requestAnimationFrame(AC_GAME_ANIMATION);
+}
+requestAnimationFrame(AC_GAME_ANIMATION);
+
---
+++ static/js/src/playground/particle/zbase.js
@@ -0,0 +1,40 @@
+class particle extends GameObjects{
+    constructor(playground, x, y, vx, vy, radius, speed, color, move_length){
+        super();
+        this.playground = playground;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.radius = radius;
+        this.speed = speed;
+        this.color = color;
+        this.move_length = move_length;
+        this.friction = 0.9;
+        this.eps = 1;
+    }
+
+    start(){
+    }
+
+    update(){
+        if(this.speed < this.eps || this.move_length < this.eps){
+            this.destroy();
+            return false;
+        }
+        let moved = Math.min(this.move_length, this.speed * this.timedelta / 1000);
+        this.x += moved * this.vx;
+        this.y += moved * this.vy;
+        this.speed *= this.friction;
+        this.move_length -= moved;
+        this.render();
+    }
+
+    render(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+    }
+}
